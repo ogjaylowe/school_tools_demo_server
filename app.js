04b59feead055a5ab0552a0ef1e3e478ff6caeb1
Jay Lowe <jaylowe@jays-mbp.lan>
2020-06-29 12:07:24 -0700

    cors 3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 // mongoDB connection via mongoose
 var configValues = require('./config.js')
@@ -35,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
 var cors = require('cors')
 
-// cors origin URL - Allow inbound traffic from origin
-corsOptions = {
-    origin: "https://ogjaylowe.github.io/school_tools_frontend_fresh/",
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
-  app.use(cors(corsOptions));
 
 
 // mongoDB connection via mongoose
@@ -23,22 +16,38 @@ mongoose.connect(configValues.db), {
 // mongo Schema to be used
 var Admin = require('../models/Admin.model')
 
+
+
+// cors origin URL - Allow inbound traffic from origin
+corsOptions = {
+    origin: "https://ogjaylowe.github.io/school_tools_frontend_fresh/",
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+router.options('https://school-server-demo.herokuapp.com/admin', cors()) // enable pre-flight request for POST request
+router.use(cors(corsOptions));
+
+router.get('https://school-server-demo.herokuapp.com/admin', cors(corsOptions), function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for only example.com.' })
+})
+
+/*
 // get Schema information from mongo db
 router.get('/', function (req, res) {
-    Admin.find({})
-        .exec(function (err, admin) {
-            if (err) {
-                res.send('error!')
-            } else {
-                res.json(admin)
-            }
-        })
+  Admin.find({})
+      .exec(function (err, admin) {
+          if (err) {
+              res.send('error!')
+          } else {
+              res.json(admin)
+          }
+      })
 });
+*/
 
 // check for a parent -- if none found return false (indicates not a user)
 router.post('/', function (req, res) {
     console.log(req.body)
-    Admin.findOne({ username: req.body.username, password: req.body.password})
+    Admin.findOne({ username: req.body.username, password: req.body.password })
         .exec(function (err, admin) {
             if (err) {
                 res.send('error!')

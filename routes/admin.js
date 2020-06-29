48d77f5172f43198f207942117b3fb8db31b8052
Jay Lowe <jaylowe@jays-mbp.lan>
2020-06-29 12:48:02 -0700

    6

--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,7 @@ var Admin = require('../models/Admin.model')
 
 // cors origin URL - Allow inbound traffic from origin
 corsOptions = {
-    origin: "https://ogjaylowe.github.io/school_tools_frontend_fresh/",
+    origin: "https://ogjaylowe.github.io",
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -29,32 +29,34 @@ corsOptions = {
 router.options('/', cors()) // enable pre-flight request for POST request
 router.use(cors(corsOptions));
 
+/*
 router.get('/', cors(corsOptions), function (req, res, next) {
     res.json({ msg: 'This is CORS-enabled for only example.com.' })
 })
+*/
 
-/*
 // get Schema information from mongo db
-router.get('/', function (req, res) {
+router.get('/', cors(corsOptions), function (req, res) {
   Admin.find({})
       .exec(function (err, admin) {
           if (err) {
-              res.send('error!')
+              res.send(err)
           } else {
               res.json(admin)
           }
       })
 });
-*/
+
 
 // check for a parent -- if none found return false (indicates not a user)
 router.post('/', cors(corsOptions), function (req, res) {
-    console.log(req.body)
+    console.log("POST body --> ", req.body)
     Admin.findOne({ username: req.body.username, password: req.body.password })
         .exec(function (err, admin) {
             if (err) {
-                res.send('error!')
+                res.send(err)
             } else if (!admin) {
+                console.log("no admin with cred found")
                 res.send(false)
             } else {
                 console.log(admin)
